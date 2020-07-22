f00d442436f8d7a7eb208884d455369257a1e147
Basant kumar Hota <basahota@users.noreply.github.com>
2020-07-22 13:29:09 +0530

    Add files via upload

---
+++ src/main/java/com/javatechie/aws/lambda/PhoneServiceHandler.java
@@ -0,0 +1,6 @@
+package com.javatechie.aws.lambda;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class PhoneServiceHandler extends SpringBootRequestHandler<String,Object> {
+}
